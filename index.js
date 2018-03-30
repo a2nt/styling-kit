95e402cc8efd7db94a10a9d2a9f450bb9d060586
Tony Air <tony@thewebdevelopmentagency.com>
2018-03-31 04:34:58 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('\n\n\n##################################################');
+console.log('Styling kit starting up ..');
+console.log('--------------------------------------------------\n');
+
+var gnome = require('./gnome-theme');
+gnome.start('default');
+
+console.log('\n--------------------------------------------------');
+console.log('Styling kit is ready!');
+console.log('##################################################\n\n\n');
